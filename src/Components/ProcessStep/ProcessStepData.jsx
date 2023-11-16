0226a916a744978b084b8887d00017318c89f7d3
sabari245 <sabari2004@hotmail.com>
2023-11-16 14:48:33 +0530

    initial commit

---
+++ src/Components/ProcessStep/ProcessStepData.jsx
@@ -0,0 +1,108 @@
+const ProcessStepData01 = [
+    {
+        icon: "line-icon-Idea-5",
+        title: "Legacies Forum",
+        content: "A Publishing Company",
+    },
+    {
+        icon: "line-icon-Fountain-Pen",
+        title: "Lead With Legacies",
+        content: "An Ed-Tech Company",
+    },
+    {
+        icon: "line-icon-Loading-2",
+        title: "Legacies Navigate",
+        content: "Press Release Division",
+    },
+    {
+        icon: "line-icon-Juice",
+        title: "Legacies Press",
+        content: "Print Division",
+    },
+]
+
+const ProcessStepData02 = [
+    {
+        title: "Project research",
+        content: "Lorem ipsum amet consectetur do eiusmod tempor incididunt",
+    },
+    {
+        title: "Sketches and design",
+        content: "Lorem ipsum amet consectetur do eiusmod tempor incididunt",
+    },
+    {
+        title: "Seleted concept",
+        content: "Lorem ipsum amet consectetur do eiusmod tempor incididunt",
+    }
+]
+
+const ProcessStepData03 = [
+    {
+        title: "Legacies Forum",
+        content: "A Publishing Company",
+    },
+    {
+        title: "Lead With Legacies",
+        content: "An Ed-Tech Company",
+    },
+    {
+        title: "Legacies Navigate",
+        content: "Press Release Platform",
+    },
+    {
+        title: "Legacies Press",
+        content: "The Print Division",
+    },
+]
+
+const ProcessStepData04 = [
+    {
+        title: "RESEARCH PROJECT",
+        content: "Lorem ipsum dolor consectetur adipiscing eiusmod tempor",
+    },
+    {
+        title: "FIND PROBLEM",
+        content: "Lorem ipsum dolor consectetur adipiscing eiusmod tempor",
+    },
+    {
+        title: "START OPTIMIZE",
+        content: "Lorem ipsum dolor consectetur adipiscing eiusmod tempor",
+    },
+    {
+        title: "REACH TARGET",
+        content: "Lorem ipsum dolor consectetur adipiscing eiusmod tempor",
+    },
+]
+
+const ProcessStepData05 = [
+    {
+        title: "Start market research",
+        content: "Lorem ipsum is simply text of the printing and typesetting industry lorem ipsum",
+    },
+    {
+        title: "Discussion of the idea",
+        content: "Lorem ipsum is simply text of the printing and typesetting industry lorem ipsum",
+    },
+    {
+        title: "Production planning",
+        content: "Lorem ipsum is simply text of the printing and typesetting industry lorem ipsum",
+    }
+]
+
+const ProcessStepData06 = [
+    {
+        title: "Innovative business",
+        content: "Easy to customize",
+    },
+    {
+        title: "Expertly marketing",
+        content: "High quality services",
+    },
+    {
+        title: "Engaging audiences",
+        content: "Build perfect websites",
+    }
+]
+
+
+export { ProcessStepData01, ProcessStepData02, ProcessStepData03, ProcessStepData04, ProcessStepData05, ProcessStepData06 }
